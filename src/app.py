0afdc4445e90b6b94b856a9b261a3c89d2d6c84b
Jasur Okhunov <jsr.okh@gmail.com>
2025-05-22 11:24:04 -0400

    Add CORS

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from src.classifier import classify_file
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) 
 
 ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpeg', 'jpg', 'docx', 'xlsx', 'csv', 'txt'}
 
